e48a67867218f4f3fb6103b3c7dbbf08752e2d38
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2015-10-09 11:50:01 -0430

    A better result
    - Now, the result is a real Object because of 'node-instagram-analytics' stops using 'JSON.stringify()
    
    - Minor changes in code style

--- index.js
+++ index.js
@@ -1,54 +1,40 @@
 var Instagram = require('./lib/instagram');
 
 function Analytics (token) {
-
     if (typeof token !== 'string') {
         var error = new Error('Invalid token');
         return console.log(error);
     }
-
     this.token = token;
     this.ig = new Instagram(this.token);
 }
 
 Analytics.prototype.simple = function (user, callback) {
-
     var self = this;
-
     var cb = function (err, response) {
         if (err) {
             var error = new Error(err);
             return callback(error);
         }
-
         self.ig.getMedias(response.id, function (err, analytics) {
-
             if (err) {
                 var error = new Error(err);
                 return console.log(error);
             }
-
-            var result;
-            var data = {};
+            var result = {};
             var media = response.counts.media;
             var follows = response.counts.follows;
             var followed_by = response.counts.followed_by;
-
-            data.likes_per_media = analytics.likes / media;
-            data.comments_per_media = analytics.comments / media;
-            data.total_likes = analytics.likes;
-            data.total_comments = analytics.comments;
-            data.ratio = followed_by / follows;
-            data.limits = analytics.limits;
-            data.calls = analytics.calls;
-
-            result = JSON.stringify(data, undefined, 2);
+            result.likes_per_media = analytics.likes / media;
+            result.comments_per_media = analytics.comments / media;
+            result.total_likes = analytics.likes;
+            result.total_comments = analytics.comments;
+            result.ratio = followed_by / follows;
+            result.limits = analytics.limits;
+            result.calls = analytics.calls;
             callback(null, result);
-
         });
-
     }
-
     if (typeof user === 'number') {
         self.ig.getProfile(user, cb);
     } else if (typeof user === 'string') {
@@ -57,9 +43,7 @@ Analytics.prototype.simple = function (user, callback) {
                 var error = new Error(err);
                 return callback(error);
             }
-
             self.ig.getProfile(uid, cb);
-
         });
     } else {
         var error = new Error('Invalid user');
